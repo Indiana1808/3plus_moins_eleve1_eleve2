199388423024503616433023f71690a3f985bb84
Indiana1808 <jenitatindiana@yahoo.com>
2020-07-13 21:19:51 +0400

    v0.2

---
+++ plusmoins2/plusmoins.c
@@ -0,0 +1,132 @@
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ 
+ 
+int main(int argc, char *argv[])
+{
+ 
+   
+    
+    int nombreMystere = 0, nombreEntre = 0, nombreCoups = 0, continuerPartie = 1, niveauDifficulte = 0, MAX = 1, modejeu = 0;
+    const int MIN = 1;
+ 
+    do
+    {
+        nombreCoups = 0;
+        printf("Choisissez un mode de jeu : \n");
+        printf("1. Mode solo\n");
+        printf("2. Mode multijoueur\n");
+ 
+        do
+        {
+ 
+        printf("Votre choix ? ");
+        scanf("%d", &modejeu);
+        }while(modejeu < 1 || modejeu > 2);
+        switch(modejeu)
+ 
+        {
+        case 1:
+    srand(time(NULL));
+    printf("----------------\n");
+        break;
+ 
+        case 2:
+    printf("\nChoisissez un nombre a faire deviner a votre adversaire : ");
+    scanf("%d", &nombreMystere);
+        }
+ 
+        if(modejeu == 1)
+        {
+ 
+        printf("Choisissez un niveau de difficulte : \n");
+        printf("1. Niveau facile : Entre 1 et 100\n");
+        printf("2. Niveau moyen : Entre 1 et 1000\n");
+        printf("3. Niveau difficile : Entre 1 et 10000\n");
+ 
+        do
+        {
+ 
+        printf("Votre choix ? ");
+        scanf("%d", &niveauDifficulte);
+        }while(niveauDifficulte < 1 || niveauDifficulte > 3);
+ 
+        printf("-----------------\n");
+ 
+        if(niveauDifficulte == 1)
+        {
+ 
+        MAX = 100;
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+ 
+        }
+ 
+        else if(niveauDifficulte == 2)
+ 
+        {
+ 
+        MAX = 1000;
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+ 
+        }
+ 
+        else if(niveauDifficulte == 3)
+ 
+        {
+ 
+        MAX = 10000;
+        nombreMystere = (rand() % (MAX - MIN + 1)) + MIN;
+ 
+         }
+ 
+        }
+    system("cls");
+    printf("\n");
+    printf("Devinez le nombre mystere !\n");
+ 
+    do
+    {
+        nombreCoups++;
+        printf("Quel est le nombre ? ");
+        scanf("%d", &nombreEntre);
+        if(nombreEntre < nombreMystere)
+ 
+           {
+ 
+            printf("C'est plus !\n");
+ 
+           }
+ 
+        else if(nombreEntre > nombreMystere)
+ 
+        {
+ 
+        printf("C'est moins !\n");
+ 
+        }
+ 
+        else
+ 
+        {
+ 
+        printf("\nBravo, vous avez trouve le nombre mystere en %d coups\n", nombreCoups);
+ 
+        }
+    }while(nombreEntre != nombreMystere);
+ 
+    printf("\n------------------------------------\n");
+    printf("Souhaitez-vous refaire une partie ?\n");
+    printf("1. Oui\n");
+    printf("2. Non\n");
+    printf("Votre choix ? ");
+    scanf("%d", &continuerPartie);
+    printf("-------------------------------------\n\n");
+ 
+    }while(continuerPartie == 1);
+ 
+    return 0;
+}
+
